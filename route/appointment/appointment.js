f4921c0725879d2fb4350d8232b74ce89ff451c9
dipto33 <ekramdipto@gmail.com>
2022-01-07 20:04:03 +0600

    Merge branch 'master' of E:\4-2\Thesis\Thesis_update\Block-Chain-Part-Thesis with conflicts.

--- route/appointment/appointment.js
+++ route/appointment/appointment.js
@@ -2,29 +2,12 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const mysql = require('mysql');
-const Web3 = require('web3');
-const NodeRSA = require("node-rsa");
-const Firebase = require('firebase')
-
-const key = new NodeRSA({b: 512});
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAGWCTeMiFUG2c1sKdbUEZB5zhGTLawXQs",
-    authDomain: "medichain-e0025.firebaseapp.com",
-    databaseURL: "https://medichain-e0025.firebaseio.com",
-    projectId: "medichain-e0025",
-    storageBucket: "medichain-e0025.appspot.com",
-    messagingSenderId: "444071301435",
-    appId: "1:444071301435:web:867a53d6663e828e8f671e",
-    measurementId: "G-T57GVVFM1Z"
-};
-
 
 const connection = mysql.createConnection({
-    host: process.env.SQL_HOST_NAME,
-    user: process.env.SQL_USER_NAME,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'medi'
 });
 
 const storage = multer.diskStorage({
@@ -39,11 +22,10 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage, limits: 1024 * 1024 * 5});
 
 
-/*router.get('/', function (req, res) {
-    ///not work
+router.get('/', function (req, res) {
+    console.log("APPOINTMENT")
     const id = req.query.id;
     const userName = req.query.user_name;
-
     res.cookie('id', id);
     res.render('appointment', {
         data: {
@@ -52,26 +34,18 @@ const upload = multer({storage: storage, limits: 1024 * 1024 * 5});
         }
     })
 
-})*/
+})
 
 router.post('/', upload.single('imagefile'), function (req, res) {
     let contact = req.body.contact;
     let details = req.body.details;
     let cloudAddress = req.body.cloud_address;
-    let image = process.env.HOST + (process.env.PORT | 3000) + "/" + req.file.path
+    //let image = 'http://localhost:3000/' + req.file.path
     let userType = req.cookies.user_type;
     let userName = req.cookies.user_name;
-    let walletAddress = req.cookies.wallet_address;
     let id = req.cookies.id;
 
-    console.log(walletAddress)
-    console.log(req.cookies.private_key)
-
-
-    if ((contact === undefined || contact === "") || (details === undefined || details === "") || (cloudAddress === undefined || cloudAddress === ""))
-        res.redirect(req.get('host'));
-
-    if (userType === 'doctor') {
+    if (userType == 'doctor') {
         connection.query('SELECT * FROM `doctor` WHERE `user_name` = ?', [userName], function (error, results, fields) {
             if (error) {
                 res.send(error)
@@ -84,18 +58,17 @@ router.post('/', upload.single('imagefile'), function (req, res) {
                         if (insertError) {
                             res.send(insertError)
                         }
-                        res.cookie('user_name', userName);
-                        res.cookie('isLogin', true);
+                        res.cookie('', userName);
+                        res.cookie('isLoguser_namein', true);
                         res.cookie('user_type', 'patient');
-                        connection.query('INSERT INTO `appointment` (`user_name`, `id`, `time`, `details`, `contact`,`image`) VALUES (?, ?, ?, ?,?,?);',
-                            [userName, id, new Date(), details, contact, image],
+                        connection.query('INSERT INTO `appointment` (`user_name`, `id`, `time`, `details`, `contact`) VALUES (?, ?, ?, ?, ?);',
+                            [userName, id, new Date(), details, contact],
                             function (appointmentError, insertResult, fields) {
                                 if (appointmentError) {
                                     res.send(appointmentError)
                                 }
-                                if (storeCloudAddressToBlockChain(cloudAddress, walletAddress)) {
-                                    res.redirect('/home')
-                                }
+                                ///TODO:: store cloud address hashed in the block chain
+                                res.redirect('/home')
                             })
 
                     })
@@ -105,139 +78,18 @@ router.post('/', upload.single('imagefile'), function (req, res) {
         });
 
     } else {
-        connection.query('INSERT INTO `appointment` (`user_name`, `id`, `time`, `details`, `contact`,  `image`) VALUES (?,?, ?, ?, ?, ?);',
-            [userName, id, new Date(), details, contact, image],
+        connection.query('INSERT INTO `appointment` (`user_name`, `id`, `time`, `details`, `contact`) VALUES (?, ?, ?, ?, ?);',
+            [userName, id, new Date(), details, contact],
             function (appointmentError, insertResult, fields) {
                 if (appointmentError) {
                     res.send(appointmentError)
                 }
                 ///TODO:: store cloud address hashed in the block chain
-                if (storeCloudAddressToBlockChain(cloudAddress, walletAddress)) {
-                    res.redirect('/home')
-                }
-
+                res.redirect('/home')
             })
     }
 
 
 })
-
-
-async function storeCloudAddressToBlockChain(cloudAddress, walletAddress) {
-
-    const app = Firebase.initializeApp(firebaseConfig)
-    const db = app.firestore()
-
-    let success = false;
-    let publicKeyObj = new NodeRSA(walletAddress);
-    let encryptedData = publicKeyObj.encrypt(cloudAddress, 'base64').toString();
-
-    const web3 = new Web3(process.env.WEB3_PORT_ADDRESS)
-    const deplyedABI = [
-        {
-            "inputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-        },
-        {
-            "constant": true,
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "name": "data",
-            "outputs": [
-                {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "constant": true,
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "addr",
-                    "type": "string"
-                }
-            ],
-            "name": "getData",
-            "outputs": [
-                {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "constant": false,
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "addr",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_data",
-                    "type": "string"
-                }
-            ],
-            "name": "setData",
-            "outputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ];
-    const deplyedAddress = process.env.DEPLOYED_ADDRESS;
-
-    const contract = new web3.eth.Contract(
-        deplyedABI, deplyedAddress
-    );
-    /*contract.methods.getData("b").call().then(
-        function (data) {
-            console.log(data);
-            console.log('data');
-        }
-    )*/
-
-    await web3.eth.getAccounts().then(async function (accounts) {
-        let acc = accounts[0]
-
-        await db.collection('cyper').add({
-            'encrypted_data': encryptedData,
-        }).then((doc) => {
-            console.log(doc.path)
-            const encryptedAddress = doc.path.split('/')[1];
-            console.log(encryptedAddress)
-            contract.methods.setData(walletAddress.toString().split('\n')[2], encryptedAddress).send({from: acc}).then(function (tnx) {
-                console.log(tnx)
-                console.log(walletAddress.toString().split('\n')[2])
-                console.log(encryptedAddress)
-            }).catch(function (err) {
-                console.log(err)
-            })
-        })
-
-
-    })
-    return success;
-
-}
-
 
 module.exports = router
